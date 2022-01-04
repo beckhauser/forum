7bc607ef25f33ea1b3bec2b4cb06c53e836005d6
Beckhauser <beckhauser125@gmail.com>
2022-01-04 20:02:04 -0300

    Criando CursoRepository

---
+++ src/main/java/br/com/beckhauser/forum/repository/CursoRepository.java
@@ -0,0 +1,9 @@
+package br.com.beckhauser.forum.repository;
+
+import br.com.beckhauser.forum.models.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    Curso findByNome(String nome);
+}
