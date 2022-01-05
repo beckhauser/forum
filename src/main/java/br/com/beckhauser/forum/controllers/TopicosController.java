e47dc2be60f76dd32ab6ee73a5497aef62567289
Beckhauser <beckhauser125@gmail.com>
2022-01-04 21:11:18 -0300

    Adicionando retorno ao método cadastrar

--- src/main/java/br/com/beckhauser/forum/controllers/TopicosController.java
+++ src/main/java/br/com/beckhauser/forum/controllers/TopicosController.java
@@ -6,8 +6,11 @@ import br.com.beckhauser.forum.models.Topico;
 import br.com.beckhauser.forum.repository.CursoRepository;
 import br.com.beckhauser.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,9 +36,11 @@ public class TopicosController {
     }
 
     @PostMapping
-    public void cadastrar(@RequestBody TopicoForm form){
+    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder){
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
 
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
     }
 }
