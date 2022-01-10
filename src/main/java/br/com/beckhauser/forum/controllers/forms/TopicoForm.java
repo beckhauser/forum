e6bd52f5a44b6a769f9e86b7a05cbc0e2ca7e118
Beckhauser <beckhauser125@gmail.com>
2022-01-10 20:39:36 -0300

    Adicionando validaçoes no TopicoForm

--- src/main/java/br/com/beckhauser/forum/controllers/forms/TopicoForm.java
+++ src/main/java/br/com/beckhauser/forum/controllers/forms/TopicoForm.java
@@ -4,11 +4,25 @@ import br.com.beckhauser.forum.models.Curso;
 import br.com.beckhauser.forum.models.Topico;
 import br.com.beckhauser.forum.repository.CursoRepository;
 import br.com.beckhauser.forum.repository.TopicoRepository;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 public class TopicoForm {
 
+    @NotNull
+    @NotEmpty
+    @Length(min = 5)
     private String titulo;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 10)
     private String mensagem;
+
+    @NotNull
+    @NotEmpty
     private String nomeCurso;
 
     public String getTitulo() {
