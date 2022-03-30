4eae5d69cf714c3c5fa02a0b214d13387ea8fcbe
Beckhauser <beckhauser125@gmail.com>
2022-03-30 19:55:11 -0300

    Changing method converter, List to Page

--- src/main/java/br/com/beckhauser/forum/controllers/DTO/TopicoDTO.java
+++ src/main/java/br/com/beckhauser/forum/controllers/DTO/TopicoDTO.java
@@ -1,6 +1,7 @@
 package br.com.beckhauser.forum.controllers.DTO;
 
 import br.com.beckhauser.forum.models.Topico;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,8 +21,8 @@ public class TopicoDTO {
         this.dataCriacao = topico.getDataCriacao();
     }
 
-    public static List<TopicoDTO> converter(List<Topico> topicos) {
-        return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+    public static Page<TopicoDTO> converter(Page<Topico> topicos) {
+        return topicos.map(TopicoDTO::new);
     }
 
     public Long getId() {
